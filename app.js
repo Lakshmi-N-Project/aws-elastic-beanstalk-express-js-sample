b888c294a522d0743f3e00f53cdaf53a1d173b45
lakshmi.n <lakshmi.n@mvgdigital.com>
2021-02-26 15:48:26 +0530

    New edited code

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!....This is my first hand on aws..Hey..!!I have created the first Pipeline on AWS..'));
-app.get('/', (req, res) => res.send('Hello World!....This is my first hand on aws..Hey..!!I have created the first Pipeline on AWS..'));
+app.get('/', (req, res) => res.send('Hello World!....This is my first hand on aws..I have created the first Pipeline on AWS..'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
