d09148d87ddfac65fa79d0e88a8c029f285473c0
lakshmi.n <lakshmi.n@mvgdigital.com>
2021-02-26 13:44:47 +0530

    Entered Message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!....This is my first hand on aws..Hey..!!I have created the first Pipeline on AWS..'));
+app.get('/', (req, res) => res.send('Hello World!....This is my first hand on aws..Hey..!!I have created the first Pipeline on AWS..'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
